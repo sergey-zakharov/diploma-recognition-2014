aa6997657a3881d0839f71c13db9181780f5258f
sergey-zakharov <serge9339@gmail.com>
2014-06-04 11:32:13 +0400

    more improvements

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import knnClassifier as cl
 import quality_assessment as qa
 import cv2
+import sys
 
 from subprocess import call
 
@@ -11,8 +12,17 @@ if __name__ == '__main__':
 	classifier.train()
 
 	# testing
-	test_filename = "./test_data/originals/101.jpg"
-	original_text_file  = "./test_data/originals/101.txt"
+	if len(sys.argv) != 2:
+		print "Pass 1 argument - path to image, please"
+		print 'there are ' + str(len(sys.argv)) + ' params'
+		print 'Params: '+ str(sys.argv)
+		exit(0)
+	
+	file_name = sys.argv[1]
+	file_raw_name = file_name.split('.')[-2].split('/')[-1]
+	test_filename = "./test_data/originals/" + file_raw_name + ".jpg"
+	original_text_file  = "./test_data/originals/" + file_raw_name + ".txt"
+
 	print "Testing " + test_filename
 	im = cv2.imread(test_filename)
 	#cv2.imshow('resultImage',im)
@@ -28,11 +38,6 @@ if __name__ == '__main__':
 	ret, resimg = cv2.threshold(gray, thres, 255, cv2.THRESH_BINARY)
 	
 	cv2.imshow('resultImage',resimg)
-	while(1):
-		k = cv2.waitKey(1) #& 0xFF
-		if k == 27:
-			break
-	cv2.destroyAllWindows()
 
 	# prepare files
 	save_path = "./test_data/" + test_filename.split('/')[-1].split('.')[0] + "-proc.jpg"
@@ -44,11 +49,11 @@ if __name__ == '__main__':
 	#call_cat = ["cat", "./test_data/" + recognize_path+".txt"]
 	#call(call_cat)
 
-	original_text = ""
-	original_text_file = open(original_text_file, 'r+')
-	for line in original_text_file:
-		original_text+=line
-	print "Original text:", original_text
+	#original_text = ""
+	#original_text_file = open(original_text_file, 'r+')
+	#for line in original_text_file:
+	#	original_text+=line
+	#print "Original text:", original_text
 	
 	recognized_text = ""
 	recognized_text_file = open(recognize_path+".txt", 'r+')
@@ -57,5 +62,13 @@ if __name__ == '__main__':
 	print "Recognized text:", recognized_text
 	# assess
 
-	print "Assessment:", qa.getRatio(original_text, recognized_text)
+	#print "Assessment:", qa.getRatio(original_text, recognized_text)
+
+	while(1):
+		k = cv2.waitKey(1) #& 0xFF
+		if k == 27:
+			break
+	cv2.destroyAllWindows()
+
+	
 
--- manual_bin_select.py
+++ manual_bin_select.py
@@ -1,6 +1,7 @@
 import cv2
 import numpy as np
 import sys
+import os
 from subprocess import call
 
 img = []
@@ -14,14 +15,22 @@ threshold_type_name_before_switch = "cv2.THRESH_BINARY"
 image_dict = {}
 image_directory = ""
 globalblurRate = 0
+isInverted = False
 
 def updateImage(threshold):
     global img
     global resimg
     global global_threshold
     global is_init_image
+    global globalblurRate
+
     global_threshold = threshold = cv2.getTrackbarPos('threshold','image')
-    ret, resimg = cv2.threshold(img,threshold,255,cv2.THRESH_BINARY)
+    param = cv2.getTrackbarPos('param','image')
+    #ret, resimg = cv2.threshold(img,threshold,255,cv2.THRESH_BINARY)
+    blur = img
+    if globalblurRate != 0:
+        blur = cv2.GaussianBlur(img,(globalblurRate, globalblurRate),0)
+    resimg = cv2.adaptiveThreshold(blur, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, threshold, param)
     cv2.imshow('image',resimg)
     is_init_image = False
 
@@ -38,11 +47,10 @@ def thresholdSwitcher():
         return
     resimg = img = cv2.imread(sys.argv[1],0)
     cv2.namedWindow('image', cv2.WINDOW_NORMAL)
-    print img
-    # create trackbars for color change
-    cv2.createTrackbar('threshold','image',0,255,updateImage)
+    cv2.createTrackbar('threshold','image',0, 10000,updateImage)
+    cv2.createTrackbar('param','image',0, 1000,updateImage)
+    cv2.createTrackbar('gaussianBlur','image', 3, 21, updateImageForGaussianBlurChange)
     cv2.imshow('image',img)
-    
     try:
         print "press 's' to recognize and save "
         print "press 'esc' to exit"
@@ -60,6 +68,7 @@ def thresholdSwitcher():
                 else:
                     ending = "-thres"
                 save_path = '/'.join(splitted_path[:-1]) + "/thresholded/" + filename + "-thres.jpg"
+                print save_path
                 recognize_path = '/'.join(splitted_path[:-1]) + "/thresholded/" + filename + ending
                 ret = cv2.imwrite(save_path,resimg) #sys.argv[1][:-4] + "-thres.jpg"
                 if ret == 1:
@@ -91,15 +100,19 @@ def updateImageForSelector(threshold):
     global global_threshold
     global is_init_image
     global threshold_type
+    global isInverted
 
     global_threshold = threshold = cv2.getTrackbarPos('threshold','image')
     blur = img
     if globalblurRate != 0:
         blur = cv2.GaussianBlur(img,(globalblurRate, globalblurRate),0)
-    if threshold_type == cv2.ADAPTIVE_THRESH_MEAN_C:
-        resimg = cv2.adaptiveThreshold(blur, 255, threshold_type, cv2.THRESH_BINARY, 7, threshold)
-    else:
-        ret, resimg = cv2.threshold(blur, threshold, 255, threshold_type)
+    if isInverted:
+        blur = (255 - blur)
+    #print threshold_type
+    #if threshold_type == cv2.ADAPTIVE_THRESH_MEAN_C:
+    #    resimg = cv2.adaptiveThreshold(blur, 255, threshold_type, cv2.THRESH_BINARY, 7, threshold)
+    #else:
+    ret, resimg = cv2.threshold(blur, threshold, 255, threshold_type)
     cv2.imshow('image',resimg)
     is_init_image = False
 
@@ -135,6 +148,7 @@ def updateImageForOtsuThresChange(otsu_is_on):
     global threshold_type
     global threshold_type_name
     global globalblurRate
+    global isInverted
     #otsu_is_on = cv2.getTrackbarPos('threshold','image')
     if otsu_is_on:
         threshold_type += cv2.THRESH_OTSU
@@ -183,23 +197,69 @@ def updateImageDict():
     image_map_file = open('./'+image_directory+'/image_map', 'r+')
     for line in image_map_file:
         splitted = line.split(' ')
-        image_dict[splitted[0]] = splitted[1].replace('\n', '')
+        print splitted
+        image_dict[splitted[0]] = int(splitted[1].replace('\n', ''))
+
+def removeImageFromDict(image_directory, real_name, name, ending):
+    # remove files
+    delete_path = "./"+image_directory+"/" + name + ".jpg"
+    bin_type_path = "./"+image_directory+"/" + name + "-method.txt"
+    recognize_path = "./"+image_directory+"/" + name + "-recog-result" + ending + ".txt"
+    try:
+        print "deleting", delete_path
+        os.remove(delete_path)
+    except OSError:
+        print "no file", delete_path
+    try:
+        print "deleting", bin_type_path
+        os.remove(bin_type_path)
+    except OSError:
+        print "no file", bin_type_path
+    try:
+        print "deleting", recognize_path
+        os.remove(recognize_path)
+    except OSError:
+        print "no file", recognize_path
+
+    # remove from index
+    f = open('./'+image_directory+'/image_map',"r")
+    lines = f.readlines()
+    f.close()
+    f = open('./'+image_directory+'/image_map',"w")
+    for line in lines:
+        if line.find(real_name) == -1:
+            f.write(line)
+    f.close()
 
 def getNewNameFromImageDict():
     global image_dict
     try:
-        return str(int(image_dict[max(image_dict, key=image_dict.get)]) + 1)
+        max_num = int(image_dict[max(image_dict, key=image_dict.get)])
+        new_num = max_num + 1
+        return str(new_num)
     except ValueError:
         return 1
 
 def writeToImageDict(filename):
     global image_dict
     new_name = str(getNewNameFromImageDict())
+    print "new_name", new_name
     image_dict[filename] = new_name
     with open("./"+image_directory+"/image_map", "a") as myfile:
-        myfile.write(filename + " " + new_name +"\n")
+        myfile.write(filename + " " + str(new_name) +"\n")
     updateImageDict()
-    return new_name
+    return str(new_name)
+
+def updateInvertImage(val):
+    global resimg
+    global isInverted
+
+    if val == 1:
+        isInverted = True
+    else:
+        isInverted = False
+    resimg = (255 - resimg)
+    cv2.imshow('image',resimg)
 
 def manualThresholdTypeSelector():
     # Create a black image, a window
@@ -211,17 +271,19 @@ def manualThresholdTypeSelector():
     global image_dict
     global image_directory
 
+    threshold_type = cv2.THRESH_BINARY
     if len(sys.argv) != 2:
         print "Pass 1 argument - path to image, please"
         print 'there are ' + str(len(sys.argv)) + ' params'
         print 'Params: '+ str(sys.argv)
         return
     resimg = img = cv2.imread(sys.argv[1],0)
-    cv2.namedWindow('image', cv2.WINDOW_NORMAL)
+    cv2.namedWindow('image', cv2.WINDOW_AUTOSIZE)
     cv2.createTrackbar('threshold','image', 0, 255, updateImageForSelector)
-    cv2.createTrackbar('isOtsu','image', 0, 1, updateImageForOtsuThresChange)
+    cv2.createTrackbar('invert','image', 0, 1, updateInvertImage)
+    #cv2.createTrackbar('isOtsu','image', 0, 1, updateImageForOtsuThresChange)
     cv2.createTrackbar('gaussianBlur','image', 3, 21, updateImageForGaussianBlurChange)
-    cv2.createTrackbar('adaptiveThres','image', 0, 1, updateImageForThresAdaptiveChange)
+    #cv2.createTrackbar('adaptiveThres','image', 0, 1, updateImageForThresAdaptiveChange)
     # TODO create several checkboxes for adaptive types
     cv2.imshow('image',img)
     splitted_path = sys.argv[1].split('/')
@@ -232,21 +294,27 @@ def manualThresholdTypeSelector():
     try:
         print "press 's' to recognize and save "
         print "press 'esc' to exit"
+
+        full_name = filename
+        if full_name not in image_dict:
+            print image_dict
+            name = writeToImageDict(filename)
+        else:
+            name = str(image_dict[filename])
         while(1):
-            
             k = cv2.waitKey(1) #& 0xFF
             if k == 27:
                 break
+            elif k == 114: # 'r'
+                ending = ''
+                if is_init_image:
+                    ending = "-init"
+                #remove image from set
+                removeImageFromDict(image_directory, filename, name, ending)
             elif k == 115: # 's'
                 ending = ''
                 if is_init_image:
                     ending = "-init"
-                full_name = filename
-                if full_name not in image_dict:
-                    print image_dict
-                    name = writeToImageDict(filename)
-                else:
-                    name = image_dict[filename]
                 save_path = "./"+image_directory+"/" + name + ".jpg"
                 bin_type_path = "./"+image_directory+"/" + name + "-method" # ex: cv2.THRESH_BINARY 123
                 recognize_path = "./"+image_directory+"/" + name + "-recog-result" + ending
@@ -264,7 +332,7 @@ def manualThresholdTypeSelector():
                     print('-------------------------------')
                     line = '-------------------------------'
                     threshold_text = 'Threshold: ' + threshold_type_name +' ' + str(global_threshold) + "/255"
-                    threshold_type_text = threshold_type_name +' ' + str(global_threshold) + "\nblur: " + str(globalblurRate)
+                    threshold_type_text = threshold_type_name +' ' + str(global_threshold) + "\nblur: " + str(globalblurRate) + "\ninverted: " + str(int(isInverted))
                     with open(recognize_path+'.txt', "a") as myfile:
                         myfile.write(line + '\n' + threshold_text)
                     #myfile.closed
@@ -279,5 +347,5 @@ def manualThresholdTypeSelector():
     cv2.destroyAllWindows()
 
 #testSwitchersExample()
-#thresholdSwitcher()
-manualThresholdTypeSelector()
+thresholdSwitcher()
+#manualThresholdTypeSelector()
